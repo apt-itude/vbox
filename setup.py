c00608691238f85f3664a64cc73afa7567556510
Alexander Thompson <athompson@128technology.com>
2016-12-13 13:47:56 -0500

    Updated version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='vbox',
-    version='0.0.1',
+    version='0.0.2',
     description='VirtualBox VM Management Tool',
     packages=['vbox'],
     entry_points={
